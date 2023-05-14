c1213a4e03447a7d739390a7a56f8fa6603c5221
Kusal Wijekoon <79883649+iamkw97@users.noreply.github.com>
2023-05-14 11:49:25 +0530

    register ui is modified

--- resources/views/app/auth/login.blade.php
+++ resources/views/app/auth/login.blade.php
@@ -29,13 +29,10 @@
                                         <input type="submit" id="send_message" value="Sign In" class="btn-main btn-fullwidth rounded-3" />
                                     </div>
                                 </form>
-                                <div class="title-line">Or&nbsp;sign&nbsp;up&nbsp;with</div>
+                                <div class="title-line">Or&nbsp;sign&nbsp;up</div>
                                 <div class="row g-2">
-                                    <div class="col-lg-6">
-                                        <a class="btn-sc btn-fullwidth mb10" href="#"><img src="{{asset('welcome/images/svg/google_icon.svg')}}" alt="">Google</a>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <a class="btn-sc btn-fullwidth mb10" href="#"><img src="{{asset('welcome/images/svg/facebook_icon.svg')}}" alt="">Facebook</a>
+                                    <div class="col-lg-12">
+                                        <a class="btn-sc btn-fullwidth mb10" href="{{route('register.index')}}"><img src="{{asset('welcome/images/svg/user.png')}}" alt="">Sign up</a>
                                     </div>
                                 </div>
                             </div>
--- resources/views/app/auth/register.blade.php
+++ resources/views/app/auth/register.blade.php
@@ -31,8 +31,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
-                    <h3>Don't have an account? Register now.</h3>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <h3>Don't have an account?&nbsp; Sign up now.</h3>
 
                     <div class="spacer-10"></div>
 
@@ -42,51 +41,79 @@
 
                             <div class="col-md-6">
                                 <div class="field-set">
-                                    <label>Name:</label>
-                                    <input type='text' name='name' id='name' class="form-control">
+                                    <label for='fullname'>Full Name<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='text' name='fullname' id='fullname' class="form-control">
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="field-set">
-                                    <label>Email Address:</label>
+                                    <label for='email'>Email Address<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
                                     <input type='text' name='email' id='email' class="form-control">
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="field-set">
-                                    <label>Choose a Username:</label>
-                                    <input type='text' name='username' id='username' class="form-control">
+                                    <label for='address'>Address<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='text' name='address' id='address' class="form-control">
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="field-set">
-                                    <label>Phone:</label>
-                                    <input type='text' name='phone' id='phone' class="form-control">
+                                    <label for='city'>City<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='text' name='city' id='city' class="form-control">
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="field-set">
-                                    <label>Password:</label>
-                                    <input type='text' name='password' id='password' class="form-control">
+                                    <label for='phone1'>Phone 1<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='text' name='phone1' id='phone1' class="form-control">
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="field-set">
-                                    <label>Re-enter Password:</label>
-                                    <input type='text' name='re-password' id='re-password' class="form-control">
+                                    <label for='phone2'>Phone 2</label>
+                                    <input type='text' name='phone2' id='phone2' class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="field-set">
+                                    <label for='dob'>Date of Birth<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='date' name='dob' id='dob' class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="field-set">
+                                    <label for='license'>Personal ID/Driving License<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='text' name='license' id='license' class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="field-set">
+                                    <label for='password'>Password<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='password' name='password' id='password' class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="field-set">
+                                    <label for='repassword'>Re-enter Password<span style="color:rgb(255, 0, 0)">&nbsp;*</span></label>
+                                    <input type='password' name='repassword' id='repassword' class="form-control">
                                 </div>
                             </div>
 
 
                             <div class="col-md-12">
 
-                                <div id='submit' class="pull-left">
-                                    <input type='submit' id='send_message' value='Register Now' class="btn-main color-2">
+                                <div id='submit' class="pull-right">
+                                    <input type='submit' id='send_message' value='Sign up' class="btn-main color-2">
                                 </div>
 
                                 <div id='mail_success' class='success'>Your message has been sent successfully.</div>
